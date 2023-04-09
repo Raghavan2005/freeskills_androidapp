12f463859e6eab88c8cb26dcc5a6935fcf8ad9b3
Raghavan <raghavan.work@outlook.com>
2023-04-09 12:34:28 +0530

    Added

---
+++ app/src/main/java/in/softface/raghavan/freeskills/SplashScreenActivity.java
@@ -0,0 +1,17 @@
+package in.softface.raghavan.freeskills;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        finish();
+    }
+}
